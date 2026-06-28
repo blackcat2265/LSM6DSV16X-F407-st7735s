7b7208bd5c9fd0953cb6f46baa339a6399283c8c
blackcat2265 <145097867+blackcat2265@users.noreply.github.com>
2026-06-28 23:50:16 +0700

    Add files via upload

--- Core/Inc/imu_fifo.h
+++ Core/Inc/imu_fifo.h
@@ -3,19 +3,18 @@
 
 #include "lsm6dsv16x_reg.h"
 
+typedef struct {
+    float lux, luy, luz;    // Акселерометр (g)
+    float lgx, lgy, lgz;    // Гироскоп (dps)
+    float pitch, roll;      // Углы SFLP
+    float q[4];             // Кватернион SFLP
+    uint16_t fifo_level;    // Текущая заполненность
+    uint8_t last_tag;       // Последний обработанный тег
+} imu_data_t;
+
+extern imu_data_t imu_data; // Сделаем данные доступными для main.c
+
+// Прототип функции обслуживания FIFO
 void imu_fifo_service(stmdev_ctx_t *ctx);
 
-extern uint8_t imu_last_tag;
-extern uint16_t imu_fifo_level;
-
-#endif
-
-#pragma once
-
-#include "lsm6dsv16x_reg.h"
-
-extern volatile uint8_t  fifo_irq;
-extern volatile uint8_t  imu_last_tag;
-extern volatile uint16_t imu_fifo_level;
-
-void imu_fifo_service(stmdev_ctx_t *ctx);
+#endif /* IMU_FIFO_H */
