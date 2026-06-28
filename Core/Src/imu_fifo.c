2b8081df13438756085095113b3e55555f725e6a
blackcat2265 <145097867+blackcat2265@users.noreply.github.com>
2026-06-28 23:51:50 +0700

    Add files via upload

--- Core/Src/imu_fifo.c
+++ Core/Src/imu_fifo.c
@@ -1,49 +1,41 @@
 #include "imu_fifo.h"
-
-uint8_t imu_last_tag = 0;
-uint16_t imu_fifo_level = 0;
-
+#include <string.h>
+imu_data_t imu_data; // Глобальная структура для хранения последних данных
 void imu_fifo_service(stmdev_ctx_t *ctx)
 {
-    lsm6dsv16x_fifo_status_t fifo;
-    lsm6dsv16x_fifo_out_raw_t raw;
+  lsm6dsv16x_fifo_status_t fifo_status;
+  uint8_t tag;
+  uint8_t data[1];
+  uint16_t samples = 0;
 
-    lsm6dsv16x_fifo_status_get(ctx, &fifo);
+  // 1. Проверяем, сколько данных накопилось в FIFO
+  if (lsm6dsv16x_fifo_status_get(ctx, &fifo_status) != 0) {
+    return;
+  }
 
-    imu_fifo_level = fifo.fifo_level;
+  samples = fifo_status.fifo_level;
 
-    if (imu_fifo_level == 0)
-        return;
-
-    lsm6dsv16x_fifo_out_raw_get(ctx, &raw);
-
-    imu_last_tag = raw.tag;
-}
-
-#include "imu_fifo.h"
-
-volatile uint8_t  fifo_irq = 0;
-volatile uint8_t  imu_last_tag = 0;
-volatile uint16_t imu_fifo_level = 0;
-
-void imu_fifo_service(stmdev_ctx_t *ctx)
-{
-    lsm6dsv16x_fifo_status_t fifo_status;
-
-    lsm6dsv16x_fifo_status_get(ctx, &fifo_status);
-
-    imu_fifo_level = fifo_status.fifo_level;
-
-    while (fifo_status.fifo_level)
-    {
-        lsm6dsv16x_fifo_out_raw_t fifo_raw;
-
-        lsm6dsv16x_fifo_out_raw_get(ctx, &fifo_raw);
-
-        imu_last_tag = fifo_raw.tag;
-
-        fifo_status.fifo_level--;
+  // 2. Вычитываем FIFO пакет за пакетом
+  while (samples--) {
+    // Чтение тега и 6 байт данных (автоматически сдвигает указатель FIFO в датчике)
+    if (lsm6dsv16x_fifo_out_raw_get(ctx, &tag, data) != 0) {
+      break;
     }
+
+    // Обработка данных в зависимости от тега (подготовка под SFLP)
+    switch (tag) {
+      case LSM6DSV16X_XL_NC_TAG:
+        // Здесь будет обработка акселерометра из FIFO
+        break;
+      case LSM6DSV16X_GY_NC_TAG:
+        // Здесь будет обработка гироскопа из FIFO
+        break;
+      case LSM6DSV16X_SFLP_GY_QUAT_TAG:
+        // Здесь будет обработка кватерниона SFLP
+        break;
+      default:
+        // Прочие теги игнорируем
+        break;
+    }
+  }
 }
-
-
