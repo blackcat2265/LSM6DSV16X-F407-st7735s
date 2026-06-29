7963f2aa0f33f64fd6730e0fb99b8c2e0d02d5d2
blackcat2265 <145097867+blackcat2265@users.noreply.github.com>
2026-06-29 14:49:17 +0700

    Add files via upload

--- Core/Inc/imu.h
+++ Core/Inc/imu.h
@@ -2,6 +2,6 @@
 #define IMU_H
 #include "lsm6dsv16x_reg.h"
 
-extern stmdev_ctx_t dev_ctx;
+extern stmdev_ctx_t dev_ctx; // Теперь все, кто инклудит imu.h, видят эту переменную
 void imu_init(void);
 #endif
